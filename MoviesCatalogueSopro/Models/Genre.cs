3d49ca10246e72b818aefb321702beb10e01910f
trnkovskii <trnkovskiilija@yahoo.com>
2022-05-19 16:02:16 +0200

    update 1

--- MoviesCatalogueSopro/Models/Genre.cs
+++ MoviesCatalogueSopro/Models/Genre.cs
@@ -10,7 +10,7 @@ namespace MoviesCatalogueSopro.Models
     {
         [Display(Name = "Id")]
         public int GenreId { get; set; }
-        [Display(Name = "Genre Name")]
+        [Display(Name = "Genre")]
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Movie> Movies { get; set; }
